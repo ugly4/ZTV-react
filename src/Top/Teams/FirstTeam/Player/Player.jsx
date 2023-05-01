149e717810cbd8976080e49624a7b15ca504ae49
Kirill Simovin <volce.chat@mail.ru>
2023-05-01 19:06:35 +0400

    Some optimization

--- src/Top/Teams/FirstTeam/Player/Player.jsx
+++ src/Top/Teams/FirstTeam/Player/Player.jsx
@@ -3,14 +3,14 @@ import "./Player.css"
 
 function Player(props){
     return(
-        <div class="player_top">
-            <div class="players_background">
-                <div class="crop">
-                    <img id="top_team_player1" src={props.photo} alt={props.nick} />
+        <div className="player_top">
+            <div className="players_background">
+                <div className="crop">
+                    <img src={props.photo} alt={props.nick} />
                 </div>
             </div>
-            <div class="nick">
-                <img id="top_team_player1_flag" src={props.flagPath} alt={props.country} />
+            <div className="nick">
+                <img src={props.flagPath} alt={props.country} />
                 <p>{props.nick}</p>
             </div>
         </div>
